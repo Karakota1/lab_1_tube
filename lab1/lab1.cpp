7c0479803d32ab261ff751fd17f08995bb0e6ec3
Karakota1 <140350621+Karakota1@users.noreply.github.com>
2023-09-28 00:53:42 +0300

    Экземпляр каждой структуры теперь один(

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -2,6 +2,9 @@
 #include <vector>
 #include <Windows.h>
 #include <winuser.h>
+#include <cstdlib>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
@@ -9,140 +12,264 @@ using namespace std;
 struct Pipe {
 	string name;
 	int length;
+	int diameter;
+	bool isWorking = true;
 };
 
 struct CS {
 	string name;
 	int wrkshopsCount;
 	vector <bool> WS;
-	float efficiency = 100;
+	float efficiency;
 };
-
-vector <Pipe> pipeMas;
-vector <CS> CSs;
 
 int Menu() {
 	int number;
-	std::cout << "\n1.Добавить труду\n2.Добавить КС\n3.Список Труб\n4.Список Кс\n5.Редактировать трубу\n6.Редактировать Кс\n7.Сохранить\n8.Загрузить\n0.Выход\n\n";
+	std::cout << "\n1.Добавить трубу\n2.Добавить КС\n3.Список объектов\n4.Редактировать трубы\n5.Редактировать цеха\n6.Сохранить\n7.Загрузить\n0.Выход\n\n";
 	std::cin >> number;
 	return number;
 }
 
-void AddPipe() {
-	int n;
-	cout << "\nВведите число труб: ";
-	cin >> n;
-	for (int i=0; i < n; i++) {
-		string name;
-		int length;
-		cout << "Введите имя и длину трубы:\n";
-		cin >> name >> length;
-		pipeMas.push_back({ name, length });
-	}
-}
-
-void ViewPipes() {
-	cout << "Список труб:\n";
-	for (int i = 0; i < pipeMas.size(); i++) {
-		cout << "Порядковый номер: " << i + 1 << endl;
-		cout << "Название трубы: " << pipeMas[i].name << endl;
-		cout << "Длина трубы: " << pipeMas[i].length << endl << endl;
-	}
-}
-void AddCS() {
-	int n;
-	cout << "\nВвидите число Кс: ";
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		string name;
-		int count;
-		vector <bool> WSs;
-		cout << "Ввидите название КС и число его цехов:\n";
-		cin >> name >> count;
-		if (count > 0) {
-			for (int i=0; i < count; i++) {
-				int n;
-				cout << "\nСтанция работает исправно?\n\n1.Да\n2.Нет\n\n";
-				cin >> n;
-				if (n == 1) {
-					WSs.push_back(true);
-				}
-				else if (n == 2) {
-					WSs.push_back(false);
-				}
-				else {
-					cout << "Неверно введен ответ";
-				}
-			}
-		}
-		else {
-			continue;
-		}
-		CSs.push_back({ name, count, WSs,100 });
-	}
-}
-
-void ViewCSs() {
-	for (int i = 0; i < CSs.size(); i++) {
-		cout << "Название КС: " << CSs[i].name << endl;
-		cout << "Число Цехов: " << CSs[i].wrkshopsCount << endl << endl;
-		for (int j = 0; j < CSs[i].WS.size(); j++) {
-			if (CSs[i].WS[j])
-				cout << "Цех №" << j << "      " << "В рабочем состоянии" << endl;
-			else
-				cout << "Цех №" << j << "      " << "Не в рабочем состоянии" << endl;
-		}
-	}
-}
-
-void PipeChange() {
-	int n;
-	int k;
+void AddPipe(Pipe& pipe) {
 	string name;
 	int length;
-	if (pipeMas.size() > 0) {
-		ViewPipes();
-		cout << "\n\tВыберете трубу для редактирования: ";
-		cin >> n;
-		cout << "\n\nВыдерете, что будете редактировать:\n1.Название\n2.Длина\n";
-		cin >> k;
-		if (k == 1) {
-			cin >> name;
-			pipeMas[n - 1].name = name;
+	int diameter;
+	cout << "Введите название, длину и ширину трубы:\n";
+	cin >> name >> length >> diameter;
+	pipe = { name, length,diameter, true };
+}
+
+void ViewPipes(Pipe pipe) {
+	if (pipe.length > 0) {
+		cout << "Название трубы: " << pipe.name << endl;
+		cout << "Длина трубы: " << pipe.length << endl;
+		cout << "Диаметр трубы: " << pipe.diameter << endl;
+		if (pipe.isWorking)
+			cout << "Состояние: ВКЛ." << endl;
+		else
+			cout << "Состояние: ВЫКЛ." << endl;
+	}
+}
+void AddCS(CS& cs) {
+	string name;
+	int count;
+	vector <bool> WSs;
+	cout << "Ввидите название КС и число его цехов:\n";
+	cin >> name >> count;
+	if (count > 0) {
+		for (int j=0; j < count; j++) {
+			int n;
+			cout << "\nСтанция работает исправно?\n\n1.Да\n2.Нет\n\n";
+			cin >> n;
+			if (n == 1) {
+				WSs.push_back(true);
+			}
+			else if (n == 2) {
+				WSs.push_back(false);
+			}
+			else {
+				cout << "Неверно введен ответ";
+				j--;
+			}
 		}
-		else if (k == 2) {
-			cin >> length;
-			pipeMas[n - 1].length = length;
+	}
+	cs = { name, count, WSs, 100};
+}
+
+void ViewCSs(CS& cs) {
+	if (cs.WS.size() != 0) {
+		cout << "Название КС: " << cs.name << endl;
+		cout << "Число Цехов: " << cs.wrkshopsCount << endl << endl;
+		cout << "Эффективность КС: " << cs.efficiency << endl << endl;
+		for (int j = 0; j < cs.WS.size(); j++) {
+			if (cs.WS[j])
+				cout << "\tЦех №" << j + 1 << "      " << "В рабочем состоянии" << endl;
+			else
+				cout << "\tЦех №" << j + 1 << "      " << "Не в рабочем состоянии" << endl;
+		}
+		cout << endl;
+	}
+}
+
+void PipeChange(Pipe& pipe) {
+	int n;
+	cout << "Состояние:\n1.Работает\n2.Требуется ремонт\n";
+	cin >> n;
+	switch (n) {
+		case 1:
+			pipe.isWorking = true;
+		case 2:
+			pipe.isWorking = false;
+	}
+}
+
+void CSChange(CS& cs) {
+	int n;
+	int w;
+	string name;
+	if (cs.wrkshopsCount > 0) {
+		ViewCSs(cs);
+		cout << "\n\tВыберете Цех для редактирования: ";
+		cin >> w;
+		if (cs.WS.size() > w) {
+			cout << "\n\nOut of range\n\n";
+		}
+		else {
+			if (cs.WS[w - 1])
+				cs.WS[w - 1] = false;
+			else
+				cs.WS[w - 1] = true;
 		}
 	}
 }
 
-void CSChange() {
-
+void SavePipesData(Pipe& pipe) {
+	ofstream file;
+	file.open("pipes.txt", fstream::in | ofstream::out);
+	if (!file.is_open()) {
+		system("cls");
+		cout << "Ошибка чтения файла!";
+	}
+	if(pipe.length > 0 and pipe.diameter > 0) {
+		file << pipe.name << " " << pipe.length << " " << pipe.diameter << " " << pipe.isWorking;
+	}
+	else {
+		file << "pipe" << " " << 0 << " " << 0 << " " << 1;
+	}
+	file.close();
+	system("cls");
+	cout << "*Файл успешно сохранен\n";
 }
 
+void SaveCSsData(CS& cs) {
+	ofstream file;
+	file.open("CSs.txt", fstream::in | ofstream::out);
+	if (!file.is_open()) {
+		system("cls");
+		cout << "Ошибка чтения файла!";
+	}
+	file << cs.name << " " << cs.wrkshopsCount << endl;
+	file.close();
+
+	file.open("WS.txt", fstream::in | ofstream::out);
+	if (!file.is_open()) {
+		system("cls");
+		cout << "Ошибка чтения файла!";
+	}
+	if(cs.WS.size()>0){
+		for (int i = 0; i < cs.WS.size(); i++) {
+			if (cs.WS[i]) {
+				file << 1 << " ";
+			}
+			else {
+				file << 0 << " ";
+			}
+			file << endl;
+		}
+		file.close();
+	}
+	else {
+		cout << "Ошибка";
+	}
+	
+	system("cls");
+	cout << "*Файл успешной сохранен\n";
+}
+
+void SaveData(Pipe pipe,CS cs) {
+	SavePipesData(pipe);
+	SaveCSsData(cs);
+}
+
+void LoadDataCS() {
+	/*ifstream Ws("C:/repo/lab_1_tube/saved_CSs/WS.txt");
+	std::string line;
+	std::string lineWs;
+	int i=0;
+	if(Ws.is_open()){
+		while (std::getline(Ws, lineWs)) {//Заходит на линию и через for разбивает эту линию на отдельные элементы
+			vector <bool> WSS;
+			for (int j; j < lineWs.size(); j++) {//Добавление цехов, а точнее булевого вектора, содержащего состояние
+				if (lineWs[j] == 1) {
+					WSS.push_back(true);
+				}
+				else if (lineWs[j] == 2) {
+					WSS.push_back(false);
+				}
+			}
+			CSs[i].WS = WSS;
+			i++;
+		}
+	}
+	else {
+		cout << "\nОшибка чтения\n";
+	}
+	Ws.close();
+
+	ifstream file("C:/repo/lab_1_tube/saved_CSs/CSs.txt");
+	if (!file.is_open()) {
+		cout << "Ошибка чтения";
+	}else {
+		while (std::getline(file, line)) {//Добавление названия и кол-ва цехов
+			CSs[i].name = line[0];
+			CSs[i].wrkshopsCount = line[1];
+		}
+	}
+	file.close();*/
+}
+
+void LoadDataPipe() {
+
+}
 
 int main()
 {
+	Pipe pipe;
+	CS cs;
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 	while (true) {
 		int number = Menu();
-		if (number == 1) {
-			AddPipe();
-		}
-		else if (number == 2) {
-			AddCS();
-		}
-		else if (number == 3) {
-			ViewPipes();
-		}
-		else if (number == 4) {
-			ViewCSs();
-		}
-		else if (number == 5) {
-			PipeChange();
+		switch (number) {
+		case 1:
+			system("cls");
+			AddPipe(pipe);
+			system("cls");
+			break;
+		case 2:
+			system("cls");
+			AddCS(cs);
+			system("cls");
+			break;
+		case 3:
+			system("cls");
+			ViewPipes(pipe);
+			ViewCSs(cs);
+			break;
+		case 4:
+			system("cls");
+			PipeChange(pipe);
+			system("cls");
+			break;
+		case 5:
+			system("cls");
+			CSChange(cs);
+			system("cls");
+			break;
+		case 6:
+			SaveData(pipe,cs);
+			break;
+		case 7:
+			//LoadDataCS();
+			break;
+		case 0:
+			exit(0);
+		default:
+			system("cls");
+			cout << "Ошибка ввода\n";
+			break;
 		}
 	}
 	return 0;
 }
+
